7181a3b12baabdde8a9fe153a9d24ba6139dfe4c
Elijah <elijahoneal@yahoo.com>
2020-12-16 20:34:37 -0500

    Import images

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,29 +1,32 @@
 import React from "react"
+// images
+import pasta from '../Assets/pasta.jpg'
+import healthy from '../Assets/healthy.jpg'
+import noodles from '../Assets/noodles.jpg'
+import burger from '../Assets/burger.jpg'
+import fruit from '../Assets/fruit.jpg'
+import cupcake from '../Assets/cupcake.jpg'
+// images
 
-
-
-const pasta = require('../Assets/pasta.jpg');
-// const healthy = require('../src/Assets/healthy.jpg');
-// const noodles = require('../src/Assets/noodles.jpg');
-// const burger = require('../src/Assets/burger.jpg');
-// const fruit = require('../src/Assets/fruit.jpg');
-// const cupcake = require('../src/Assets/cupcake.jpg');
 
 
 
 const Home = () => {
   return (
+
+
     
-  <section>
-    <img src = {pasta} alt="Pasta by Lisa Fotios from Pexels"></img>
-    {/* <img src = {healthy} alt="Healthy Bowl by Ella Olsson from Pexels"></img>
-    <img src = {noodles} alt="Noodles by Snapwire from Pexels"></img>
-    <img src = {burger} alt="Burger & Fries by Jonathan Borba from Pexels"></img>
-    <img src = {fruit} alt="Fruit Bowl by Trang Doan from Pexels"></img>
-    <img src = {cupcake} alt="Cupcakes by Jess Bailey Designs from Pexels"></img> */}
-  </section> 
-  )  
+  <section className = "gallery">
+      <img src = {pasta} alt="Pasta by Lisa Fotios from Pexels"></img>
+      <img src = {healthy} alt="Healthy Bowl by Ella Olsson from Pexels"></img>
+      <img src = {noodles} alt="Noodles by Snapwire from Pexels"></img>
+      <img src = {burger} alt="Burger & Fries by Jonathan Borba from Pexels"></img>
+      <img src = {fruit} alt="Fruit Bowl by Trang Doan from Pexels"></img>
+      <img src = {cupcake} alt="Cupcakes by Jess Bailey Designs from Pexels"></img>
+  </section>
+ 
+  )
     
-}
+};
 
 export default Home;
