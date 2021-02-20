98063e58f8c4766141aa64a68631b80956e89420
Elijah <elijahoneal@yahoo.com>
2021-02-20 09:38:32 -0500

    Add links to navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
-
+import { Link } from 'react-router-dom'
 const App = () => {
   return (
-    <>
+    <div id='wrapper'>
+      <header>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+     <nav>
+        <Link to='/'>Home</Link>
+        <Link to='/pizza'>Order</Link>
+     </nav>
+      </header>
+    </div>
   );
 };
 export default App;
